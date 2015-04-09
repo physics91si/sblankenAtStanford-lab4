7194f2ab563c9328a61a15d516ed963329392220
Gabriel Ehrlich <gehrlich@stanford.edu>
2015-04-09 01:12:37 -0700

    Working version of fib.py

--- fib.py
+++ fib.py
@@ -1,8 +1,38 @@
+#!/usr/bin/python
 # Physics 91SI
 # Spring 2015
 # Lab 01
 # Part 1
 # Solution
+
+import sys
+
+def main():
+    args = sys.argv[1:]
+    if len(args) == 0:
+        args = ["error"]
+    if args[0] == "help":
+        help_message = \
+"""
+
+fib.py
+
+Usage
+  - './fib.py approx n': display the n-th order Fibonacci approximation to
+    the golden ratio.
+  - './fib.py converge': keep calculating higher-order Fibonacci approximations
+    to the golden ratio until it stops changing (to floating-point precision).
+  - './fib.py help': display this help message.
+
+"""
+        print help_message
+    elif args[0] == "approx" and len(args) == 2:
+        phi_approx(int(args[1]))
+    elif args[0] == "converge" and len(args) == 1:
+        phi_converge()
+    else:
+        print "Error: input not understood.\n" \
+                "    Type './fib.py help' for info on this program."
 
 def fib(n):
     """Return nth element of the Fibonacci sequence."""
@@ -17,14 +47,38 @@ def fib(n):
         n1 = n_new
     return n0
 
-def phi_approx(n):
+phi_approx_output_format = \
+"""Approximation order: {:d}
+    fib_n: {:g}
+    fib_(n-1): {:g}
+    phi: {:.25f}"""
+
+def phi_approx(n, show_output=True):
     """Return the nth-order Fibonacci approximation to the golden ratio."""
     fib_n = fib(n)
     fib_nm1 = fib(n - 1)
     phi = float(fib_n)/fib_nm1
-    print n, fib_n, fib_nm1, phi
+    if show_output:
+        print phi_approx_output_format.format(n, fib_n, fib_nm1, phi)
     return phi
 
-# Find successively better approximations
-for i in range(2, 15):
-    print phi_approx(i)
+phi_converge_output_format = \
+"""Approximation order: {:d}
+    phi_old: {:.25f}
+    phi_new: {:.25f}"""
+
+def phi_converge():
+    """Keep calculating higher-order Fibonacci approximations to the golden
+    ratio until it stops changing (to floating-point precision)."""
+
+    i = 3
+    phi_old = phi_approx(i - 1, show_output=False)
+    phi_new = phi_approx(i)
+    while phi_old != phi_new:
+        i += 1
+        phi_old = phi_new
+        phi_new = phi_approx(i, show_output=False)
+        print phi_converge_output_format.format(i, phi_new, phi_old)
+    print "\nConverged to %.25f" % phi_new
+
+if __name__ == '__main__': main()
